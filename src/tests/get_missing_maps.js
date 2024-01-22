dc8244de574b0faa9f384a9eebb668d76fac8793
rel <92015390+mezleca@users.noreply.github.com>
2024-01-22 16:19:17 -0300

    fix brain issue
    - issue: map does not exist why not get the id of a random ass map and call it the missing beatmap id? ...

--- src/tests/get_missing_maps.js
+++ src/tests/get_missing_maps.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import PromptSync from "prompt-sync";
 
 import { OsuReader } from "../reader/reader.js";
-import { auth } from 'osu-api-extended';
+import { auth, v2, v1 } from 'osu-api-extended';
 
 /* 
 * This script will download all missing maps from your collection.db
@@ -19,7 +19,7 @@ dotenv.config({
 });
 
 // login :3
-await auth.login(process.env.OSU_ID, process.env.OSU_SECRET, ['public']);
+const login = await auth.login(process.env.OSU_ID, process.env.OSU_SECRET, ['public']);
 
 const reader = new OsuReader();
 const prompt = PromptSync();
@@ -32,6 +32,21 @@ const collection_file = fs.readFileSync(path.resolve(osu_path, "collection.db"))
 reader.set_type("osu");
 reader.set_directory("E:\\osu!");
 reader.set_buffer(osu_file);
+
+const search_map_id = async (hash) => {
+
+    const base = "https://osu.ppy.sh/api/v2/beatmaps/lookup?"
+    const response = await axios.get(`${base}checksum=${hash}`, {
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${login.access_token}`
+        }
+    })
+
+    const data = await response.data;
+    return data.beatmapset_id;
+};
 
 await reader.get_osu_data();
 
@@ -46,68 +61,79 @@ reader.set_buffer(collection_file);
 
 await reader.get_collections_data();
 
-const missing_maps = [], invalid = [];
+let missing_maps = [], invalid = [];
 const hashes = new Set(reader.osu.beatmaps.map(b => b.hash));
 
-reader.collections.beatmaps.forEach((b, i) => {
+const Maps = reader.collections.beatmaps.map((b) => { return { name: b.name, maps: b.maps } });
 
-    console.log("searching collection:", i);
-
-    b.maps.forEach((hash, i) => {
-        if (!hashes.has(hash) && !missing_maps.includes(hash)) {
-            if (reader.osu.beatmaps[i].id != "4294967295") {
-                missing_maps.push({ hash, id: reader.osu.beatmaps[i].id });
+for (const map of Maps) {
+    for (const m of map.maps) {
+        if (!hashes.has(m)) {
+            if (m != "4294967295") {
+                missing_maps.push({ hash: m });
             }
             else {
-                invalid.push({ hash, id: reader.osu.beatmaps[i].id });
+                invalid.push({ hash: m });
             }
         }
-    });
-});
+    }
+}
 
 const download_map = async (b) => {
 
     console.log("downloading map:", count + 1);
 
     try {
-        const response = await axios.get(`${base_url}${b.id}`, {
+        const response = await axios.get(`${base_url}${b}`, {
             responseType: "stream",
             method: "GET",
         });
 
-        const stream = fs.createWriteStream(path.resolve("./data/", `${b.id}.osz`));
+        const stream = fs.createWriteStream(path.resolve("./data/", `${b}.osz`));
         response.data.pipe(stream);
 
         await new Promise((resolve, reject) => {
             stream.on("finish", resolve);
             stream.on("error", (err) => {
-                console.error("error:", b.id, err);
+                console.error("error:", b, err);
                 reject(err);
             });
         });
 
-        count++;
-
     } catch (error) {
         if (error.response) {
-            console.error("api error:", error.code);
+            console.error("api error:", error.response.status, error.response.statusText);
         }
         else {
             console.log("yep that was an error...");
         }
     }
+
+    count++;
 };
 
 const download_maps = async () => {
+    
+    invalid = [];
+
     for (const map of missing_maps) {
-        await download_map(map);
+        try {
+            const hash = map.hash;
+            const id = await search_map_id(hash);
+
+            await download_map(id);
+        } catch(err) {
+            invalid.push({ hash: map.hash });
+        }    
     }
+
+    console.log(`done\nfailed to download ${invalid.length}. reason: outdated/invalid map.}`);
 };
 
 const base_url = "https://api.chimu.moe/v1/download/";
 let count = 0;
 
-console.log(`found ${missing_maps.length} missing maps\n${invalid.length} are invalid maps.`);
+console.log(`found ${missing_maps.length} missing maps\n${invalid.length} are unknown maps.`);
 
 const question = prompt("download missing maps? (y/n): ");
 
@@ -115,19 +141,21 @@ if (question != "y") {
     process.exit(0);
 }
 
-if (prompt("download from a specific collection? (y/n): ") == "y") {
 
-    const name = prompt("collection name: ");
-    missing_maps = missing_maps[name];
+// TODO
+// if (prompt("download from a specific collection? (y/n): ") == "y") {
 
-    if (!missing_maps) {
-        console.log("collection not found.");
-        process.exit(0);
-    }
-} 
+//     const name = prompt("collection name: ");
+//     missing_maps = missing_maps[name];
+
+//     if (!missing_maps) {
+//         console.log("collection not found.");
+//         process.exit(0);
+//     }
+// } 
 
 download_maps().then(() => {
     console.log("done");
-}).catch(() => {
-    console.log("something went wrong... probably with the api to download maps idk.")
+}).catch((err) => {
+    console.log("something went wrong... probably with the api to download maps idk.", err)
 });

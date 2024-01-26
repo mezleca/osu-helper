00a171b6411e6739b349c5c52e227255b3e01282
rel <92015390+mezleca@users.noreply.github.com>
2024-01-26 16:11:02 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export const config = {
     /* API info (OAuth): https://osu.ppy.sh/home/account/edit */
-    osu_id: "your_id_here",
-    osu_secret: "youd_secret_here"
+    osu_id: "",
+    osu_secret: "",
+    osu_path: "",
+    osu_songs_folder: ""
 };

50d8e34e0c3738dc30b50175f51a1689743c567c
rel <92015390+mezleca@users.noreply.github.com>
2024-04-18 09:03:58 -0300

    github filho da puta do nada para de ignorar o arquivo tomar no cu

--- src/other/config.js
+++ src/other/config.js
@@ -2,9 +2,9 @@ import { check_config } from "./utils.js";
 export const config = new Map();
 
 // Update the config files HEREE!!!!!!!!!!!!!!!:
-config.set("osu_id", "24421");
-config.set("osu_secret", "Q1qmXwZwCnD4Iao7VTjv6WNXx0NLxmPpMRvCLjf4");
-config.set("osu_path", "C:\\Users\\nameh\\AppData\\Local\\osu!");
-config.set("osu_songs_folder", "C:\\Users\\nameh\\AppData\\Local\\osu!\\Songs");
+config.set("osu_id", "");
+config.set("osu_secret", "");
+config.set("osu_path", "");
+config.set("osu_songs_folder", "");
 
 await check_config();
